1a7427bba1bed478a99c2504860b4f564212172e
LennexDev <41293242+LennexDev@users.noreply.github.com>
2018-11-18 15:28:16 +0100

    Verbessert bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+    console.log("Bot kann nun genutzt werden!")
+
+    setInterval(async function() {
+
+        let status = ['Recraft.net | -help', 'die v1', 'Recraft.net', 'Recraft.net | -help', 'auf ${bot.guilds.size} Servern'];
+        let chosen = status[Math.floor(Math.random() * status.length)];
+      
+        bot.user.setActivity(chosen, {type: "PLAYING"});
+      
+      
+      
+    }, 8000);
+});
+bot.on('message', message => {
+	if(message.content === 'help') {
+		message.channel.send('In Work! :warning:');
+   } 
+});
+bot.on('message', message => {
+	if(message.author === 'bot') {
+		message.channel.send('Der Bot ist bald 24/7 online!');
+	} 
+});
+
+bot.login(process.env.BOT_TOKEN);
