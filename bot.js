aeb4cb0d9facfd2de2f8f670207429c25c074d98
LennexDev <41293242+LennexDev@users.noreply.github.com>
2018-11-18 16:29:22 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+    console.log("Bot kann nun genutzt werden!")
+
+
+bot.on('message', message => {
+	if(message.content === 'help') {
+		message.channel.send('**Command List**');
+		message.channel.send('_bot_');
+   } 
+});
+bot.on('message', message => {
+	if(message.content === 'bot') {
+		message.channel.send('Der Bot ist bald 24/7 online!');
+	} 
+});
+bot.on('message', message => {
+	if(message.content === 'hallo') {
+		message.channel.send(':wave:');
+	} 
+});
+
+bot.login(process.env.BOT_TOKEN);
