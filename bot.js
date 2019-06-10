d924f0e2e4d36e312d5202886a55dcd68c4a793d
LennexDev <41293242+LennexDev@users.noreply.github.com>
2019-06-10 17:53:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,37 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '+ping') {
+    if (message.content === 'ping') {
     	message.channel.send(':ping_pong: Pong! 0ms.');
   	}
 });
 
-client.on('message', message => {
-	    if (message.content === '+server') {
-		message.channel.send('**==================**', 
-		'IP:. **Comming Soon!**', 
-		'Port:. **_19132_**',
-		'Status:. Online, Wartungsarbeiten ? ??', 
-        '**==================**'):
-	} 
-});
-
-client.on('message', message => {
-	if (message.content === '+botinfo') {
-		message.channel.send('Der Bot ist in der BetaPhase!'):
-		} 
-});
-
-client.on('message', message => {
-	  if (message.content === '+help') {
-		message.channel.send('**=====================**', 
-		'Prefix: *->* **+**', 
-		'__________________', 
-		'botinfo', 
-		'server', 
-		'ping'):
-	} 
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
